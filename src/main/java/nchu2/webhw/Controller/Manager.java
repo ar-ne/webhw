514e6efc27750be170ecfb6ca012ad4e35662e8a
s16201234 <zoukun971201>
2019-05-28 12:32:01 +0800

    完成导航栏

--- src/main/java/nchu2/webhw/Controller/Manager.java
+++ src/main/java/nchu2/webhw/Controller/Manager.java
@@ -1,6 +1,7 @@
 package nchu2.webhw.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static nchu2.webhw.Utils.Properites.Tamplet.Manager;
@@ -8,5 +9,10 @@ import static nchu2.webhw.Utils.Properites.Tamplet.Manager;
 @Controller
 @RequestMapping(Manager)
 public class Manager {
+    @GetMapping("/")
+    public String index() {
+        return "priv/Manager/index";
+    }
+
 
 }
--- src/main/java/nchu2/webhw/Controller/Staff.java
+++ src/main/java/nchu2/webhw/Controller/Staff.java
@@ -2,9 +2,14 @@ package nchu2.webhw.Controller;
 import static nchu2.webhw.Utils.Properites.Tamplet.*;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(Staff)
 public class Staff {
+    @GetMapping("/")
+    public String index() {
+        return "priv/Staff/index";
+    }
 }
